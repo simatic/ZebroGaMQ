0a9ebd33e14129b18a11b9d7c0b6103f8bf5f3df
Adgeg <adgeg@adgeg-laptop.(none)>
2012-04-13 14:43:48 +0200

    - GPL license added for aMazing sources.

--- game/aMazing-gamelogicserver/item.py
+++ game/aMazing-gamelogicserver/item.py
@@ -1,8 +1,30 @@
-'''
-Created on Jan 18, 2012
+"""
+aMazing! Geolocalized multiplayer game for Android devices.
+Conceived and realized within the course "Mixed Reality Games for 
+Mobile Devices" at Fraunhofer FIT (http://www.fit.fraunhofer.de).
+Copyright (C) 2012  Alexander Hermans, Tianjiao Wang
+Contact: 
+alexander.hermans0@gmail.com, tianjiao.wang@rwth-aachen.de,
+richard.wetzel@fit.fraunhofer.de, lisa.blum@fit.fraunhofer.de, 
+denis.conan@telecom-sudparis.eu, michel.simatic@telecom-sudparis.eu
 
-@author: Alex
-'''
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Developer(s): Alexander Hermans, Tianjiao Wang
+ZebroGaMQ:  Denis Conan, Gabriel Adgeg 
+"""
+
 idCounter = 0
 
 class Item(object):
--- game/aMazing-gamelogicserver/mylistofactions.py
+++ game/aMazing-gamelogicserver/mylistofactions.py
@@ -1,3 +1,30 @@
+"""
+aMazing! Geolocalized multiplayer game for Android devices.
+Conceived and realized within the course "Mixed Reality Games for 
+Mobile Devices" at Fraunhofer FIT (http://www.fit.fraunhofer.de).
+Copyright (C) 2012  Alexander Hermans, Tianjiao Wang
+Contact: 
+alexander.hermans0@gmail.com, tianjiao.wang@rwth-aachen.de,
+richard.wetzel@fit.fraunhofer.de, lisa.blum@fit.fraunhofer.de, 
+denis.conan@telecom-sudparis.eu, michel.simatic@telecom-sudparis.eu
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Developer(s): Alexander Hermans, Tianjiao Wang
+ZebroGaMQ:  Denis Conan, Gabriel Adgeg 
+"""
+
 from zebrogamq.gamelogicserver.listofactionsenumtype import ListOfActionsEnumeration
 from amazingactionkind import  AmazingActionKind
 
